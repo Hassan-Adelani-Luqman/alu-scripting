5e786a1796df89d8afc758d5ff61327f42aabad4
Hassanadelani1 <luqmanadelani1@gmail.com>
2025-01-09 00:03:37 +0200

    commit

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,15 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/School/g).join
+
+# Accepts one argument from the command line
+input = ARGV[0]
+
+# Regular expression to match 'School'
+regex = /School/
+
+# Use the match method to find the pattern in the input
+if input =~ regex
+  puts "Match found: #{input[regex]}"
+else
+  puts "No match found"
+end
+
