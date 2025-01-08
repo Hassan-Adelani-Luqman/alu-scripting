26a5d3f6eefdf1000c0067676d50a9a9a5c1e34e
Hassanadelani1 <luqmanadelani1@gmail.com>
2025-01-09 00:05:43 +0200

    commiting

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,15 +1,8 @@
 #!/usr/bin/env ruby
 
-# Accepts one argument from the command line
-input = ARGV[0]
-
-# Regular expression to match 'School'
 regex = /School/
 
-# Use the match method to find the pattern in the input
-if input =~ regex
-  puts "Match found: #{input[regex]}"
-else
-  puts "No match found"
+ARGV.each do |arg|
+  matches = arg.scan(regex)
+  puts matches.join if matches.any?
 end
-
